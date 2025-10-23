132e9781e1314efcae1df6ca5eb00e61c4ca703e
Shibam Sadhukhan <133673581+Shibam008@users.noreply.github.com>
2025-10-23 18:23:08 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (93.93%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int num(char ch) {
-        if (ch == 'I')
-            return 1;
-        else if (ch == 'V')
-            return 5;
-        else if (ch == 'X')
-            return 10;
-        else if (ch == 'L')
-            return 50;
-        else if (ch == 'C')
-            return 100;
-        else if (ch == 'D')
-            return 500;
-        else
-            return 1000;
+        switch(ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
     int romanToInt(string s) {
         int sum = 0, idx = 0;
