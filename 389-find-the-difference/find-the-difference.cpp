015ad986333cd2c1704746143aa31666dab75b4d
Shibam Sadhukhan <133673581+Shibam008@users.noreply.github.com>
2025-10-28 20:48:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (78.02%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -3,18 +3,23 @@ public:
     char findTheDifference(string s, string t) {
         vector<int> count(26, 0);
 
-        for(int i = 0; i < s.size(); i++) {
-            count[s[i] - 'a']++;
-        }
-        for(int i = 0; i < t.size(); i++) {
-            count[t[i] - 'a']--;
-        }
+        // for(int i = 0; i < s.size(); i++) {
+        //     count[s[i] - 'a']++;
+        // }
+        // for(int i = 0; i < t.size(); i++) {
+        //     count[t[i] - 'a']--;
+        // }
 
-        for(int i=0; i<26; i++) {
-            if(count[i] != 0) {
-                return i + 'a';
-            }
-        }
-        return '0';
+        // for(int i=0; i<26; i++) {
+        //     if(count[i] != 0) {
+        //         return i + 'a';
+        //     }
+        // }
+        // return '0';
+
+        char ans = 0;
+        for(char ch: s) ans ^= ch;
+        for(char ch: t) ans ^= ch;
+        return ans;
     }
 };
