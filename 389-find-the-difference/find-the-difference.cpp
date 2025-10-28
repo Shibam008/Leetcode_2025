4bce846c40479e339651a6311c218191c44ea9d7
Shibam Sadhukhan <133673581+Shibam008@users.noreply.github.com>
2025-10-28 20:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (17.61%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> count(26, 0);
+
+        for(int i = 0; i < s.size(); i++) {
+            count[s[i] - 'a']++;
+        }
+        for(int i = 0; i < t.size(); i++) {
+            count[t[i] - 'a']--;
+        }
+
+        for(int i=0; i<26; i++) {
+            if(count[i] != 0) {
+                return i + 'a';
+            }
+        }
+        return '0';
+    }
+};
