2a9879c969a6f856d665a038e9e234528b628f3e
Shibam Sadhukhan <133673581+Shibam008@users.noreply.github.com>
2025-10-30 22:29:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (12.83%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+// two pointer
     int twoPointerApproach(string haystack, string needle) 
     {
         int n = haystack.size(), m = needle.size();
@@ -24,7 +25,60 @@ public:
         }
         return -1;
     }
+
+// KMP approach
+
+    void lpsFind(vector<int>&lps, string s) 
+    {
+        int pre = 0, suf = 1;
+        while(suf < s.size()) 
+        {
+            if(s[pre] == s[suf])
+            {
+                lps[suf] = pre + 1;
+                pre++; suf++;
+            }
+            else
+            {
+                if(pre == 0) 
+                {
+                    lps[suf] = 0;
+                    suf++;
+                }
+                else 
+                {
+                    pre = lps[pre - 1];
+                }
+            }
+        }
+    }
+
     int strStr(string haystack, string needle) {
-        return twoPointerApproach(haystack, needle);
+        //return twoPointerApproach(haystack, needle);
+
+        vector<int> lps(needle.size(), 0);
+        lpsFind(lps, needle);
+        int first = 0, second = 0;
+
+        while(first < haystack.size() && second < needle.size())
+        {
+            if(haystack[first] == needle[second])
+            {
+                first++; second++;
+            }
+            else
+            {
+                if(second == 0) 
+                    first++;
+                else
+                    second = lps[second-1];
+            }
+        }
+
+        if(second == needle.size())
+        {
+            return first - second;
+        }
+        return -1;
     }
 };
