47131531c5b81107d905a1b74ca3828648ae0202
Shibam Sadhukhan <133673581+Shibam008@users.noreply.github.com>
2025-11-04 23:06:40 +0530

    Time: 3 ms (87.82%) | Memory: 15.4 MB (52.21%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.size();
+        vector<int> lps(n, 0);
+        int len = 0, i = 1;
+
+        while (i < n) {
+            if (s[i] == s[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len > 0)
+                    len = lps[len - 1];
+                else
+                    lps[i++] = 0;
+            }
+        }
+
+        int last = lps[n - 1];
+        return (last > 0 && n % (n - last) == 0);
+    }
+};
