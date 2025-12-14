bde189fd5d74c54a11cb90ee7368e737925d30b1
Shibam Sadhukhan <133673581+Shibam008@users.noreply.github.com>
2025-12-14 14:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (85.42%) - LeetSync

---
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int cnt = 0;
+
+        for(int x : arr1) {
+            bool flag = true;
+            for(int y : arr2) {
+                if(abs(x-y) <= d) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) cnt++;
+        }
+        return cnt;
+    }
+};
