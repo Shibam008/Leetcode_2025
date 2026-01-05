6e246bb77170f5613ee384a8839f5c3a9826b585
Shibam Sadhukhan <133673581+Shibam008@users.noreply.github.com>
2026-01-05 22:59:42 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (47.22%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.cpp
+++ 492-construct-the-rectangle/construct-the-rectangle.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<int> constructRectangle(int area) {
         vector<int>ans(2,0);
-        for(int i=sqrt(area); i>=1; i--) {
-            int j = area / i;
-            if(area == i*j) {
-                ans[0] = j;
-                ans[1] = i;
+        for(int width = sqrt(area); width >= 1; width--) {
+            int height = area / width;
+            if(area == height * width) {
+                ans[0] = height;
+                ans[1] = width;
                 break;
             }
         }
